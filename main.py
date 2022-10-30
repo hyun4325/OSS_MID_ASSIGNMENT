9acae49b6725f62a69d11b0a94b5d7acb1d82a60
＂huyn4325＂ <danny4865@gmail.com>
2022-10-31 07:16:32 +0900

    main뺴기 & calculation빼기 구현

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from calculation import add_function as add
+from calculation import subtract_function as sub
+
 
 print("Select operation.")
 print("1.Add")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1): ")
+    choice = input("Enter choice(1/2): ")
 
     # check if choice is one of the four options
-    if choice in ('1'):
+    if choice in ('1', '2'):
 
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
         if choice == '1':
             result = int(add.add(num1, num2))
+            print(result)
+        if choice == '2':
+            result = int(sub.sub(num1, num2))
             print(result)
 
         # check if user wants another calculation
