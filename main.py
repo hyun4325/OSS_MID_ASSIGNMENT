1f4fc2c49d18b59a5187c2299ceb505b440e396f
＂huyn4325＂ <danny4865@gmail.com>
2022-10-31 20:50:38 +0900

    main나누기 & calculation나누기 구현

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from calculation import add_function as add
 from calculation import subtract_function as sub
 from calculation import multiply_function as mul
-
+from calculation import divide_function as div
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
+print("4.Divide")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3): ")
+    choice = input("Enter choice(1/2/3/4): ")
 
     # check if choice is one of the four options3
-    if choice in ('1', '2', '3'):
+    if choice in ('1', '2', '3', '4'):
 
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
@@ -25,6 +26,9 @@ while True:
             print(result)
         elif choice == '3':
             result = int(mul.mul(num1, num2))
+            print(result)
+        elif choice == '4':
+            result = round(float(div.div(num1, num2)),2)
             print(result)
 
         # check if user wants another calculation
